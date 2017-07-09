a138ed27d0356e9b32385cf29923a9379920eb11
Kamal Mango <kamal.mango7@gmail.com>
2017-07-09 18:43:20 +0200

    complete basic setup

---
+++ src/components/organisms/Search/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        Search
+      </div>
+    )
+  }
+}
+
+export default Search
