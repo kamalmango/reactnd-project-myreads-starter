8aac22c20438b44595580b2f2f344a7b315b0d94
Kamal Mango <kamal.mango7@gmail.com>
2017-07-17 18:28:21 +0200

    add state to shelf changer

--- src/components/atoms/BookshelfChanger/index.js
+++ src/components/atoms/BookshelfChanger/index.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import './style.css'
 
 class BookshelfChanger extends Component {
+	state = {
+		value: 'currentlyReading'
+	}
+
 	handleChange = (e) => {
-		if (this.props.updateBookshelf) {
-			this.props.updateBookshelf(this.props.id, e.target.value)
-		}
+		console.log('state before ', this.state.value)
+		this.setState({value: e.target.value})
+		console.log('state after ', this.state.value)
+		//if (this.props.updateBookshelf) {
+			//this.props.updateBookshelf(this.props.id, e.target.value)
+			//console.log('state before ', this.state.value)
+			//this.setState({value: e.target.value})
+			//console.log('state after ', this.state.value)
+		//}
 	}
 
 	render() {
 		return (
 			<div className="book-shelf-changer">
-			  <select onChange={this.handleChange}>
+			  <select value={this.state.value} onChange={this.handleChange}>
 			    <option value="none" disabled>Move to...</option>
 			    <option value="currentlyReading">Currently Reading</option>
 			    <option value="wantToRead">Want to Read</option>
